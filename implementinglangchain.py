2fe6ee9b3bc30a18081b1e09d3c2435d82ad6056
Gaurav <gauravcodepro@gmail.com>
2024-07-11 12:44:31 +0200

    Update implementinglangchain.py

--- implementinglangchain.py
+++ implementinglangchain.py
@@ -1,7 +1,8 @@
 #! usr/bin/env python3
 # Author Gaurav 
 # Date 2024-6-20
-
+# you can take the literatureMiner.py and clean the tags and prepare the literature \
+                   #  if you want to put this on the other text but it will need modifications. 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import LlamaCpp
@@ -16,5 +17,3 @@ embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 model_base_explain = Qdrant.from_texts(splitted_chunks,embeddings, location=":memory:",collection_name="explain_chunks")
 
 
-# you can take the literatureMiner.py and clean the tags and prepare the literature \
-                   #  if you want to put this on the other text but it will need modifications. 
